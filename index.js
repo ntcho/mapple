cc8bf8a738963027224d3ec7384f14c98db1fcac
Nathan Cho <ntcho13@gmail.com>
2022-11-13 04:19:43 -0500

    Fix `unrecognizable font family` error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { AppRegistry } from "react-native";
-import App from "./App";
+import { registerRootComponent } from "expo";
+import App from "./src/App";
 
-AppRegistry.registerComponent("main", () => App);
+registerRootComponent(App);
 export default App;
